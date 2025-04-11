01c71bbd9a9c07b5513b6489947131d9454a6f05
topinkapecy <petanasel@seznam.cz>
2025-04-11 11:05:33 +0200

    vypracování cvičení 3

--- 105-106/3.c
+++ 105-106/3.c
@@ -1,7 +1,15 @@
 //Tvorba makra 'natreti' , které nám vypočítá třetí mocninu zadaného čísla.
 #include <stdio.h>
 
+#define NATRETI(n) ((n)*(n)*(n))
+
 int main()
 {
+    int i = 2;
+    int j = 4;
+    printf("%d\n",NATRETI(3));
+    printf("%d\n",NATRETI(i));
+    printf("%d\n",NATRETI(2+3));
+    printf("%d\n",NATRETI(i*j+1));
     return 0;
 }
