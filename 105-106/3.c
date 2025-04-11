8b155dff5121500c1ef1307e2b319264030663a3
topinkapecy <petanasel@seznam.cz>
2025-04-11 11:13:30 +0200

    úprava syntaxe

--- 105-106/3.c
+++ 105-106/3.c
@@ -1,15 +1,15 @@
-//Tvorba makra 'natreti' , které nám vypočítá třetí mocninu zadaného čísla.
+//Tvorba makra 'na_treti' , které nám vypočítá třetí mocninu zadaného čísla.
 #include <stdio.h>
 
-#define NATRETI(n) ((n)*(n)*(n))
+#define na_treti(n) ((n)*(n)*(n))
 
 int main()
 {
     int i = 2;
     int j = 4;
-    printf("%d\n",NATRETI(3));
-    printf("%d\n",NATRETI(i));
-    printf("%d\n",NATRETI(2+3));
-    printf("%d\n",NATRETI(i*j+1));
+    printf("%d\n",na_treti(3));
+    printf("%d\n",na_treti(i));
+    printf("%d\n",na_treti(2+3));
+    printf("%d\n",na_treti(i*j+1));
     return 0;
 }
